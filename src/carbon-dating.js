d44578a7dc3a0f322c27ca23b0e15f87928a8eaa
Elena Yulova <elenavy@gmail.com>
2025-10-20 09:37:38 +0300

    feat: add carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity ) {
+  if (sampleActivity &&
+    typeof sampleActivity === 'string' &&
+    typeof +sampleActivity === 'number' &&
+    +sampleActivity > 0 &&
+     +sampleActivity <= 15) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  }
+  return false
 }
 
 module.exports = {
